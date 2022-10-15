cdcb8d9e1dbb478d8da4b4f899d484a5b833ea72
Ilya Ilinykh <ilya02il@yandex.ru>
2022-10-15 20:09:25 +0700

    Добавил библиотеку классов Ilya02Il.BaseTypes.Extensions с наиболее полезными расширениями

---
+++ src/Ilya02Il.BaseTypes.Extensions/AsyncExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ilya02Il.BaseTypes.Extensions
+{
+    public static class AsyncExtensions
+    {
+        public static async Task<T> WithCancellation<T>(this Task<T> task,
+            CancellationToken cancelToken)
+        {
+            return await (Task<T>)((Task)task).WithCancellation(cancelToken);
+        }
+
+        public static async Task WithCancellation(this Task task,
+            CancellationToken cancelToken)
+        {
+            var taskCompletionSrc = new TaskCompletionSource<object>(
+                TaskCreationOptions.RunContinuationsAsynchronously);
+
+            var cancelTokenRegistration = cancelToken.Register(state =>
+                ((TaskCompletionSource<object>)state).TrySetResult(null),
+                taskCompletionSrc);
+
+            using (cancelTokenRegistration)
+            {
+                var resultTask = await Task.WhenAny(task, taskCompletionSrc.Task);
+
+                if (resultTask == taskCompletionSrc.Task)
+                    throw new OperationCanceledException(cancelToken);
+
+                await task;
+            }
+        }
+
+        public static async Task TryAsync(this Task task,
+            Action<Exception> errorHandler = null)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                errorHandler?.Invoke(ex);
+                await Task.FromException(ex);
+            }
+        }
+
+        public static async Task<T> TryAsync<T>(this Task<T> task,
+            Action<Exception> errorHandler = null)
+        {
+            return await (Task<T>)((Task)task).TryAsync(errorHandler);
+        }
+    }
+}
---
+++ src/Ilya02Il.BaseTypes.Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ilya02Il.BaseTypes.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> predicate)
+        {
+            foreach (var item in enumerable)
+                predicate(item);
+        }
+
+        public static string Join(this IEnumerable<string> sequence, string separator = "") =>
+            string.Join(separator, sequence);
+    }
+}
